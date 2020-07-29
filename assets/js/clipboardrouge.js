f56fe205c3f09b98a26716619674c35dc7519b9f
dieghernan <diego.hernangomezherrero@gmail.com>
2020-07-29 09:30:23 +0000

    Tweak clipboard.js

--- assets/js/clipboardrouge.js
+++ assets/js/clipboardrouge.js
@@ -3,8 +3,8 @@ layout: null
 ---
 // adapted from https://experimentingwithcode.com/creating-a-jekyll-blog-with-bootstrap-4-and-sass-part-4/
 $(document).ready(function() {
-   $('.highlighter-rouge').each(function(i) {
-      if (!$(this).parent().hasClass('no-select-button')) {
+   $('.highlight').each(function(i) {
+      if (!$(this).parent().hasClass('highlighter-rouge')) {
 
         // create an id for the current code section
         var currentId = "codeblock" + (i + 1);
@@ -18,7 +18,7 @@ $(document).ready(function() {
         btn.setAttribute('type', 'btn');
         btn.classList.add( 'btn-copy-code', 'text-right');
         btn.setAttribute('data-clipboard-target', '#' + currentId);
-        btn.innerHTML = '<p class="btn btn-primary btn-sm mb-0 mr-2 "><i class="far fa-copy"></i></p>';
+        btn.innerHTML = '<p class="btn btn-light btn-sm mb-0 "><i class="far fa-copy"></i></p>';
         this.insertBefore(btn, this.firstChild);
       }
     });
