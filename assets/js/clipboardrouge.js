e14c74813e65e111a641a204c274698d1dc591f7
dieghernan <diego.hernangomezherrero@gmail.com>
2020-07-29 08:56:22 +0000

    Add clipboard.js to Chulapa theme

---
+++ assets/js/clipboardrouge.js
@@ -0,0 +1,27 @@
+---
+layout: null
+---
+// adapted from https://experimentingwithcode.com/creating-a-jekyll-blog-with-bootstrap-4-and-sass-part-4/
+$(document).ready(function() {
+   $('.highlighter-rouge').each(function(i) {
+      if (!$(this).parent().hasClass('no-select-button')) {
+
+        // create an id for the current code section
+        var currentId = "codeblock" + (i + 1);
+
+        // find the code section and add the id to it
+        var codeSection = $(this).find('code');
+        codeSection.attr('id', currentId);
+
+        // now create the button, setting the clipboard target to the id
+        var btn = document.createElement('div');
+        btn.setAttribute('type', 'btn');
+        btn.classList.add( 'btn-copy-code', 'text-right');
+        btn.setAttribute('data-clipboard-target', '#' + currentId);
+        btn.innerHTML = '<p class="btn btn-primary btn-sm mb-0 mr-2 "><i class="far fa-copy"></i></p>';
+        this.insertBefore(btn, this.firstChild);
+      }
+    });
+
+    new ClipboardJS('.btn-copy-code');
+  });
