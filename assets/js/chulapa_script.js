ff5087c2d75929cdaeb3a9602105da651ff7fdfc
Diego H <dieghernan@users.noreply.github.com>
2022-12-11 12:29:56 +0100

    Add js and style

--- assets/js/chulapa_script.js
+++ assets/js/chulapa_script.js
@@ -129,3 +129,53 @@ function stripHtml(html){
 window.addEventListener('load', ch_clipboard_setup);
 
 // End CopyClipboard
+
+// SideBar ToC
+
+function setupSideBar() {
+  var sT = document.getElementById("sidetoc");
+  var btn = document.getElementById("demo");
+  var body = document.getElementById("body");
+  // Create hidden overlay
+  var iDiv = document.createElement("div");
+  if (sT) {
+    btn.classList.remove("d-none");
+    btn.classList.add("has-toc");
+    body.classList.add("bs-canvas-anim");
+    iDiv.classList.add("bs-canvas-overlay", "bg-dark", "position-fixed",
+      "w-100", "h-100");
+    iDiv.setAttribute("id", "sideBarOverlay");
+    iDiv.setAttribute("onclick", "closeSideBar()");
+    body.prepend(iDiv);
+  } else {
+    document.getElementById("sideBar")
+      .remove();
+  }
+}
+window.addEventListener("load", setupSideBar);
+
+function openSideBar() {
+  var btn = document.getElementById("demo");
+  var body = document.getElementById("body");
+  btn.classList.add("d-none");
+  btn.setAttribute("aria-expanded", "true");
+  document.getElementById("sideBarOverlay")
+    .classList.add("show");
+  body.style.overflowX = "hidden";
+  body.style.marginLeft = "300px";
+  body.style.marginRight = "-300px";
+  document.getElementById("sideBar")
+    .style.marginLeft = "0";
+}
+
+function closeSideBar() {
+  var btn = document.getElementById("demo");
+  document.getElementById("sideBar")
+    .removeAttribute("style");
+  document.getElementById("sideBarOverlay")
+    .classList.remove("show");
+  document.getElementById("body")
+    .removeAttribute("style");
+  btn.classList.remove("d-none");
+  btn.setAttribute("aria-expanded", "false");
+}
