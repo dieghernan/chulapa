eb1a7ce91100390574b33bb0adf73f0674b75c2b
Diego H <diego.hernangomezherrero@gmail.com>
2022-12-14 00:23:37 +0100

    Update chulapa script

--- assets/js/chulapa_script.js
+++ assets/js/chulapa_script.js
@@ -1,35 +1,116 @@
-// Add anchors for headings
-// Adapted from Minimal Mistakes, (c) Michael Rose
-$('#maincontent').find('h1, h2, h3, h4, h5, h6').each(function() {
-  var id = $(this).attr('id');
-  if (id) {
-    var anchor = document.createElement("a");
+var d = document;
+var sT = d.getElementById("sidetoc");
+var btn = d.getElementById("demo");
+var body = d.getElementById("body");
+var main = d.getElementById("maincontent");
+var heads = main.querySelectorAll("h1, h2, h3, h4, h5 , h6");
+var els = main.querySelectorAll("pre");
 
-    anchor.classList.add('chulapa-header-link', 'ml-2', 'chulapaDateSocial');
-    anchor.href = '#' + id;
-    anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fas fa-link fa-2xs align-middle\"></i>';
+// First load sidebar
+// Create hidden overlay
+var iDiv = d.createElement("div");
+if (sT) {
+  btn.style.marginLeft = "0";
+  iDiv.classList.add("bs-canvas-overlay", "bg-dark", "position-fixed",
+    "w-100", "h-100");
+  iDiv.setAttribute("id", "sideBarOverlay");
+  iDiv.setAttribute("onclick", "closeSideBar()");
+  body.prepend(iDiv);
+} else if (btn) {
+  // ToC was requested but no toc produced
+  // Clean up DOM
+  btn.remove();
+  d.getElementById("sideBar")
+    .remove();
+}
+
+function openSideBar() {
+  btn.removeAttribute("style");
+  btn.setAttribute("aria-expanded", "true");
+  d.getElementById("sideBarOverlay")
+    .classList.add("show");
+  d.getElementById("sideBar")
+    .style.marginLeft = "0";
+}
+
+function closeSideBar() {
+  d.getElementById("sideBar")
+    .removeAttribute("style");
+  d.getElementById("sideBarOverlay")
+    .classList.remove("show");
+  btn.style.marginLeft = "0";
+  btn.setAttribute("aria-expanded", "false");
+}
+
+// Put anchors on headings
+heads.forEach(function (currentValue) {
+  thish = currentValue;
+  id = thish.id;
+  if (id) {
+    anchor = d.createElement("a");
+    sp = d.createElement("span");
+    ic = d.createElement("i");
+    anchor.classList.add("chulapa-header-link", "ml-2",
+      "chulapaDateSocial");
+    anchor.href = "#" + id;
+    sp.innerHTML = "Permalink";
+    sp.classList.add("sr-only");
+    ic.classList.add("fas", "fa-link", "fa-2xs", "align-middle");
+    anchor.append(sp, ic);
     anchor.title = "Permalink";
-    $(this).append(anchor);
+    thish.append(anchor);
+  } else {
+    console.log("Heading :" + thish.innerHTML +
+      " does not have id. Can't place anchor here");
   }
 });
 
-// Start Copy Clipboard
+// Copy Clipboard
+// Insert buttons
+
+els.forEach(function (currentValue, currentIndex) {
+  preBlock = currentValue;
+  i = currentIndex;
+  // Select first child
+  codeBlock = preBlock.firstChild;
+  // If first child is code
+  if (codeBlock.tagName.toLowerCase() == "code") {
+    // Add id to code block
+    codeBlock.setAttribute("id", "clipboard_code" + i);
+    // Create button
+    copybtn = d.createElement("button");
+    copybtn.classList.add("btn", "text-muted", "btn-sm",
+      "btn-chulapa-copy-code");
+    copybtn.innerHTML = "<i class='far fa-copy'></i>";
+    copybtn.id = "clipboard_btn" + i;
+    copybtn.type = "button";
+    copybtn.setAttribute("data-toggle", "tooltip");
+    copybtn.setAttribute("data-placement", "left");
+    copybtn.setAttribute("aria-label", "Copy code to clipboard");
+    //Function to copy to clipboard
+    copybtn.onclick = ch_copy_cliboard(i);
+    preBlock.prepend(copybtn);
+  } else {
+    console.log("No code block for\n" + preBlock.innerHTML);
+  }
+
+});
 
 // Initialize Bootstrap tooltip
-$(function() {
-  $('[data-toggle="tooltip"]')
-    .tooltip()
+$(function () {
+  $("[data-toggle='tooltip']")
+    .tooltip();
 })
 
-function showTooltip(btn, message) {
-  btn.tooltip('hide')
-    .attr('data-original-title', message)
-    .tooltip('show');
+function showTooltip(thisbtn, message) {
+  thisbtn.tooltip("hide")
+    .attr("data-original-title", message)
+    .tooltip("show");
 }
 
-function hideTooltip(btn) {
-  setTimeout(function() {
-    btn.tooltip('hide');
+function hideTooltip(thisbtn) {
+  setTimeout(function () {
+    thisbtn.tooltip("hide");
   }, 1000);
 }
 
@@ -37,79 +118,44 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function setTooltip(btn, tooltip, style) {
-  btn.classList.remove('text-muted');
-  btn.classList.add(style);
-  btn.setAttribute('aria-label', tooltip);
+async function setTooltip(thisbtn, tooltip, style) {
+  thisbtn.classList.remove("text-muted");
+  thisbtn.classList.add(style);
+  thisbtn.setAttribute("aria-label", tooltip);
   await sleep(1000);
-  btn.classList.add('text-muted');
-  btn.setAttribute('aria-label', 'Copy code to clipboard');
-  btn.removeAttribute('data-original-title');
-  btn.classList.remove(style);
-}
-
-// End helpers tooltip
-
-// Insert buttons 
-function ch_clipboard_setup() {
-  var d = document;
-  var els = d.querySelectorAll('pre');
-  for (var i = 0; i < els.length; i++) {
-    // Select all pre codes
-    var preBlock = els[i];
-    // Select first child	
-    let codeBlock = preBlock.firstChild;
-
-    // If first child is code
-    if (codeBlock.tagName.toLowerCase() == 'code') {
-      // Add id to code block
-      codeBlock.setAttribute('id', 'clipboard_code' + i);
-      // Create button			
-      var btn = d.createElement('button');
-      btn.classList.add('btn', 'text-muted', 'btn-sm', 'btn-chulapa-copy-code');
-      btn.innerHTML = "<i class='far fa-copy'></i>";
-      btn.id = 'clipboard_btn' + i;
-      btn.type = 'button';
-      btn.setAttribute('data-toggle', 'tooltip');
-      btn.setAttribute('data-placement', 'left');
-      btn.setAttribute('aria-label', 'Copy code to clipboard');
-      //Function to copy to clipboard
-      btn.onclick = ch_copy_cliboard(i);
-      preBlock.prepend(btn);
-    } else {
-      console.log('No code block for\n' + preBlock.innerHTML);
-    }
-  }
+  thisbtn.classList.add("text-muted");
+  thisbtn.setAttribute("aria-label", "Copy code to clipboard");
+  thisbtn.removeAttribute("data-original-title");
+  thisbtn.classList.remove(style);
 }
 
 function ch_copy_cliboard(i) {
-  return function() {
-    var d = document;
+  return function () {
     // Select code content and strip HTML
-    var codeBlock = d.getElementById('clipboard_code' + i)
+    let thisCodeBlock = d.getElementById("clipboard_code" + i)
       .innerHTML;
-    codeBlockStripped = stripHtml(codeBlock);
+    codeBlockStripped = stripHtml(thisCodeBlock);
 
     // Reset clipboard
-    navigator.clipboard.writeText('');
+    navigator.clipboard.writeText("");
 
-    var btn = d.getElementById('clipboard_btn' + i);
-    var btnTrigger = $(btn);
+    thiscopybtn = d.getElementById("clipboard_btn" + i);
+    btnTrigger = $(thiscopybtn);
 
     // Set initial values
 
-    let style = 'text-success';
-    let msg = 'Copied on the clipboard:\n' + codeBlockStripped;
-    let tooltip = 'Copied!';
+    style = "text-success";
+    msg = "Copied on the clipboard:\n" + codeBlockStripped;
+    tooltip = "Copied!";
     try {
       navigator.clipboard.writeText(codeBlockStripped);
     } catch {
       // Modify on error
-      style = 'text-danger';
-      tooltip = 'Error!';
-      msg = 'Error when copying the code';
+      style = "text-danger";
+      tooltip = "Error!";
+      msg = "Error when copying the code";
     }
-    setTooltip(btn, tooltip, style);
+    setTooltip(thiscopybtn, tooltip, style);
     showTooltip(btnTrigger, tooltip);
     hideTooltip(btnTrigger);
     console.log(msg);
@@ -117,58 +163,7 @@ function ch_copy_cliboard(i) {
 }
 // stripHtml safely
 function stripHtml(html) {
-  let tmp = document.createElement('DIV');
+  let tmp = document.createElement("DIV");
   tmp.innerHTML = html;
-  return tmp.textContent || tmp.innerText || '';
+  return tmp.textContent || tmp.innerText || "";
 }
-
-window.addEventListener('load', ch_clipboard_setup);
-
-// End CopyClipboard
-
-// SideBar ToC
-
-function setupSideBar() {
-  var sT = document.getElementById("sidetoc");
-  var btn = document.getElementById("demo");
-  var body = document.getElementById("body");
-  // Create hidden overlay
-  var iDiv = document.createElement("div");
-  if (sT) {
-    btn.style.marginLeft = "0";
-    iDiv.classList.add("bs-canvas-overlay", "bg-dark", "position-fixed",
-      "w-100", "h-100");
-    iDiv.setAttribute("id", "sideBarOverlay");
-    iDiv.setAttribute("onclick", "closeSideBar()");
-    body.prepend(iDiv);
-  } else if (btn) {
-  
-    // ToC was requested but no toc produced
-    // Clean up DOM
-    btn.remove();
-    document.getElementById("sideBar")
-      .remove();
-  }
-}
-window.addEventListener("load", setupSideBar);
-
-function openSideBar() {
-  var btn = document.getElementById("demo");
-  btn.removeAttribute('style');
-  btn.setAttribute("aria-expanded", "true");
-  document.getElementById("sideBarOverlay")
-    .classList.add("show");
-  document.getElementById("sideBar")
-    .style.marginLeft = "0";
-}
-
-function closeSideBar() {
-  var btn = document.getElementById("demo");
-  document.getElementById("sideBar")
-    .removeAttribute("style");
-  document.getElementById("sideBarOverlay")
-    .classList.remove("show");
-  btn.style.marginLeft = "0";
-  btn.setAttribute("aria-expanded", "false");
-}
-
