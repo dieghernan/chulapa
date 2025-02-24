8cc51b2f5d2dd2f19eb121b1e9c4a8b7cc4d2075
Diego H. <diego.hernangomezherrero@gmail.com>
2025-02-24 10:52:47 +0100

    Giving up

--- _plugins/count_by_item.rb
+++ _plugins/count_by_item.rb
@@ -3,14 +3,14 @@
 
 module Jekyll
     module TagFilters
-        def count_by_item(input, property)
+        def count_by_item(input)
             input = input.is_a?(String) ? input.split(',') : input
             counts = Hash.new(0)
             input.flatten.each do |name|
                 name = name.strip
                 counts[name] += 1
             end
-            sorted_counts = counts.sort_by { |property, count| -count }
+            sorted_counts = counts.sort_by { |tag, count| -count }
             sorted_counts.to_h
         end
     end
