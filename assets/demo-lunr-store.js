67ad4c19c349b497640325f9d37eec39673f2297
dieghernan <dieghernan@users.noreply.github.com>
2025-02-28 20:17:53 +0000

    Deploying to gh-pages from @ dieghernan/chulapa@2778a0228cba6cd91d257b4c9066d0941baf9363 🚀

---
+++ assets/demo-lunr-store.js
@@ -0,0 +1,203 @@
+/* Demo for docs. Don't use this script on production */
+
+var store = [{
+        "title": "<span class=\"chulapa\">Chulapa</span>",
+        "subtitle": "A full flexible Jekyll theme for Github Pages",
+        "excerpt":
+            "Latest release {{ site.data.version.release }} Read the docs Easy to install Create a fully responsive website hosted on Github in minutes - for free! First-class features Fontawesome, comments, internal search,...",
+        "categories": null,
+        "date": null,
+        "tags": null,
+        "url": "https://dieghernan.github.io/chulapa/",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner.png"
+      },{
+        "title": "Welcome",
+        "subtitle": null,
+        "excerpt":
+            "Welcome to Chulapa. This is another Jekyll theme that you can use for creating your site. Motivation In the last couple of years I created some tiny websites using Github...",
+        "categories": [],
+        "date": "2020-05-15",
+        "tags": ["news","bootstrap","jekyll"],
+        "url": "https://dieghernan.github.io/chulapa/blog/20200515_welcome",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/chulapa-madrid.jpg"
+      },{
+        "title": "Plain page",
+        "subtitle": null,
+        "excerpt":
+            "This is how a plain page would look like. The layout and header type have been already set on the _config file as defaults: - scope: path: \"\" type: \"demo\"...",
+        "categories": ["demo"],
+        "date": "2025-02-28",
+        "tags": ["layout-default","header-base"],
+        "url": "https://dieghernan.github.io/chulapa/demo/01-plainpage",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Plain page with image",
+        "subtitle": "Expanding the layout",
+        "excerpt":
+            "This is how a plain page with an image would look like. An image is displayed on top of the header. Some other components are shown, as tags or social...",
+        "categories": ["demo"],
+        "date": "2025-02-28",
+        "tags": ["layout-default","header-base","image","social-links","tags","categories","bottom-navs"],
+        "url": "https://dieghernan.github.io/chulapa/demo/02-plainpage-img",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Post header",
+        "subtitle": null,
+        "excerpt":
+            "This page shows the post header with the default author set on your _config file. It also shows the date on top of the content. --- title: Post header categories:...",
+        "categories": ["demo"],
+        "date": "2020-01-01",
+        "tags": ["layout-default","header-post","social-links","tags","categories","bottom-navs","author","date"],
+        "url": "https://dieghernan.github.io/chulapa/demo/03-postheader",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Landing page",
+        "subtitle": "First impression is the last impression",
+        "excerpt":
+            "This page shows the hero header with an image with the landingpage layout. --- layout: landingpage title: Landing page subtitle: First impression is the last impression categories: [demo, landingpage] header_type:...",
+        "categories": ["demo","landingpage"],
+        "date": "2020-01-04",
+        "tags": ["related","layout-landingpage","header-hero","social-links","tags","categories","bottom-navs","date","comments","image","author"],
+        "url": "https://dieghernan.github.io/chulapa/demo/05-landinghero",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Use MathJax",
+        "subtitle": "Project page with a hero header",
+        "excerpt":
+            "This page shows the hero header with a project button. It also has enabled comments, mathjax and a modified date. Example of MathJax: \\[\\int_D ({\\nabla\\cdot} F)dV=\\int_{\\partial D} F\\cdot ndS\\] ---...",
+        "categories": ["demo"],
+        "date": "2020-02-03",
+        "tags": ["layout-default","header-hero","mathjax","social-links","tags","categories","bottom-navs","date","project-links","comments"],
+        "url": "https://dieghernan.github.io/chulapa/demo/04-heroproject",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "A guest author",
+        "subtitle": "And with a new project",
+        "excerpt":
+            "This page shows how a contributor could be added to your blog. In this case Octocat is telling us about its plans on the next months… --- title: A guest...",
+        "categories": ["demo","author-guest"],
+        "date": "2020-02-29",
+        "tags": ["layout-default","header-splash","social-links","tags","categories","bottom-navs","date","comments","project-links","author","author-guest"],
+        "url": "https://dieghernan.github.io/chulapa/demo/06-guestauthor",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Guest author",
+        "subtitle": "But no location or image",
+        "excerpt":
+            "This page shows how a contributor could be added to your blog, but without an avatar or location. Octocat is shy today. --- title: Guest author subtitle: But no location...",
+        "categories": ["demo","author-guest"],
+        "date": "2020-03-01",
+        "tags": ["layout-default","header-base","social-links","tags","categories","bottom-navs","date","comments","author","author-guest","no-avatar","no-location"],
+        "url": "https://dieghernan.github.io/chulapa/demo/07-guestauthor-no-avatar",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Layout minimal",
+        "subtitle": "A blank canvas for you",
+        "excerpt":
+            "Layout minimal A blank canvas for you Adapted from Start Bootstrap Use any Bootstrap Template! Some examples on Bootstrap Documentation, StartBootstrap or just Google it! Just use HTML Markdown files...",
+        "categories": ["demo","layout"],
+        "date": "2025-02-28",
+        "tags": ["layout-minimal","header-splash","image","project-links","bootstrap"],
+        "url": "https://dieghernan.github.io/chulapa/demo/08-minimal",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Image on header",
+        "subtitle": "Transparencies work better",
+        "excerpt":
+            "This page shows the image header. It is recommended to use a .png file with transparent background. Large images are also recommended. --- title: Image on header subtitle: Transparencies work...",
+        "categories": ["demo","layout"],
+        "date": "2025-02-28",
+        "tags": ["layout-default","header-image","image"],
+        "url": "https://dieghernan.github.io/chulapa/demo/09-image-header",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Plain page with no tags",
+        "subtitle": "Corner case",
+        "excerpt":
+            "I don’t have tags   --- title: Plain page with no tags subtitle: Corner case show_sociallinks  : true show_tags         : true show_categories   : true show_bottomnavs   : true ---  ",
+        "categories": [],
+        "date": "2025-02-28",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa/demo/10-plainpage-notags",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Related",
+        "subtitle": null,
+        "excerpt":
+            "This page shows the related snippet. See how related pages appeared on the bottom. --- title: Related categories: [demo] header_type: post tags: [layout-default,header-post, social-links, tags, categories, bottom-navs, author, date, related]...",
+        "categories": ["demo"],
+        "date": "2022-12-01",
+        "tags": ["layout-default","header-post","social-links","tags","categories","bottom-navs","author","date","related"],
+        "url": "https://dieghernan.github.io/chulapa/demo/11-related",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-demos.png"
+      },{
+        "title": "Installation",
+        "subtitle": "How to use <span class=\"chulapa\">Chulapa</span>",
+        "excerpt":
+            "Chulapa was developed in and for Github. There are three tested possible ways to use Chulapa: 1. Use our Github Template Recommended if you are starting from scratch Create a...",
+        "categories": [],
+        "date": "2025-02-28",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa/docs/01-install",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-docs.png"
+      },{
+        "title": "Global settings",
+        "subtitle": "Learn how to set up your new site",
+        "excerpt":
+            "TL;DR Learn how to modify your _config.yml file. If you are using the remote method and you didn’t fork the chulapa-101 repo, you can use this file as a starting...",
+        "categories": [],
+        "date": "2025-02-28",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa/docs/02-config",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-docs.png"
+      },{
+        "title": "Theming <span class=\"chulapa\">Chulapa</span>",
+        "subtitle": "A powerful interface to pimp your site",
+        "excerpt":
+            "Chulapa has a powerful theming interface, meaning that you could use this template for several sites and the look-and-feel of each one could be completely different. We have already introduced...",
+        "categories": [],
+        "date": "2025-02-28",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa/docs/03-theming",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-docs.png"
+      },{
+        "title": "Layouts & Snippets",
+        "subtitle": "Add your content",
+        "excerpt":
+            "Once that you have set your site and you are happy with the look, you can start creating your content. It is assumed that you are familiar with Markdown and...",
+        "categories": [],
+        "date": "2025-02-28",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa/docs/04-layouts",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-docs.png"
+      },{
+        "title": "Frequently Asked Questions",
+        "subtitle": "Some additional information you may find useful",
+        "excerpt":
+            "Additionally, there are some tips when using Chulapa, as well as code samples. How do I… …know which version of the theme I am using? If you are on remote_theme...",
+        "categories": [],
+        "date": "2025-02-28",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa/docs/05-faq",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-docs.png"
+      },{
+        "title": "Syntax highlighting demo",
+        "subtitle": "Highlighters shipped with this theme.",
+        "excerpt":
+            "This page is a demo of the different styles of syntax highlighting shipped by default with this theme. Just make your selection below. Select Theme None chulapa-skin: highlight: Python @requires_authorization...",
+        "categories": [],
+        "date": "2025-02-28",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa/docs/syntax-highlighting",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-docs.png"
+      },{
+        "title": "Markdown cheatsheet",
+        "subtitle": "The basic syntax of your page",
+        "excerpt":
+            "What’s Markdown? Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents. Created by John Gruber in 2004, Markdown is now one...",
+        "categories": [],
+        "date": "2025-02-28",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa/docs/markdown-cheatsheet",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/banner-docs.png"
+      }]
