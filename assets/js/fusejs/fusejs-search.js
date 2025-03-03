700ebdee5795aac502c6e9ca05ca3bd4d0378e4c
dieghernan <diego.hernangomezherrero@gmail.com>
2025-03-03 00:03:45 +0000

    Deploying to gh-pages from @ dieghernan/chulapa@cfef681e756f0b5e721eba0ae2f4cf67259b42f1 🚀

--- assets/js/fusejs/fusejs-search.js
+++ assets/js/fusejs/fusejs-search.js
@@ -5,7 +5,7 @@ function init() {
     const resultsContainer = document.getElementById('results');
     const resultCount = document.getElementById('resultscount');
     const options = {
-        keys: ['title', 'subtitle', 'excerpt', 'categories', 'date', 'tags'],
+        keys: ['title', 'subtitle', 'excerpt', 'content', 'categories', 'date', 'tags'],
         isCaseSensitive: false,
         useExtendedSearch: true,
         ignoreLocation: true,
@@ -44,7 +44,7 @@ function init() {
                             </div>
                             <div class="row mt-2">
                                 <div class="col">
-                                    <p>${i.item.excerpt.split(" ").splice(0, 10).join(" ").trim()}</p>
+                                    <p>${i.item.excerpt}</p>
                                 </div>
                             </div>
                             <hr>
--- assets/js/lunr/lunr-search.js
+++ assets/js/lunr/lunr-search.js
@@ -4,29 +4,30 @@ MIT License
 From minimal-mistakes: https://github.com/mmistakes/minimal-mistakes/
 */
 
-
 var inputVal = document.getElementById("search").value;
 var idx = lunr(function() {
-    this.field('title')
-    this.field('subtitle')
-    this.field('excerpt')
-    this.field('categories')
-    this.field('date')
-    this.field('tags')
-    this.ref('id')
+    this.field('title');
+    this.field('subtitle');
+    this.field('excerpt');
+    this.field('content');
+    this.field('categories');
+    this.field('date');
+    this.field('tags');
+    this.ref('id');
 
-    this.pipeline.remove(lunr.trimmer)
+    this.pipeline.remove(lunr.trimmer);
 
     for (var item in store) {
         this.add({
             title: store[item].title,
             subtitle: store[item].subtitle,
             excerpt: store[item].excerpt,
+            content: store[item].content,
             categories: store[item].categories,
             date: store[item].date,
             tags: store[item].tags,
             id: item
-        })
+        });
     }
 });
 
@@ -37,30 +38,29 @@ $(document).ready(function() {
         var resultdiv = $('#results');
         var resultcount = $('#resultscount');
         var query = $(this).val().toLowerCase();
-        var result =
-            idx.query(function(q) {
-                query.split(lunr.tokenizer.separator).forEach(function(term) {
+        var result = idx.query(function(q) {
+            query.split(lunr.tokenizer.separator).forEach(function(term) {
+                q.term(term, {
+                    boost: 100
+                });
+                if (query.lastIndexOf(" ") != query.length - 1) {
                     q.term(term, {
-                        boost: 100
-                    })
-                    if (query.lastIndexOf(" ") != query.length - 1) {
-                        q.term(term, {
-                            usePipeline: false,
-                            wildcard: lunr.Query.wildcard.TRAILING,
-                            boost: 10
-                        })
-                    }
-                    if (term != "") {
-                        q.term(term, {
-                            usePipeline: false,
-                            editDistance: 1,
-                            boost: 1
-                        })
-                    }
-                })
+                        usePipeline: false,
+                        wildcard: lunr.Query.wildcard.TRAILING,
+                        boost: 10
+                    });
+                }
+                if (term != "") {
+                    q.term(term, {
+                        usePipeline: false,
+                        editDistance: 1,
+                        boost: 1
+                    });
+                }
             });
+        });
         resultdiv.empty();
-        resultcount.empty()
+        resultcount.empty();
         if (query.length > 0) {
             if (result.length < 1) {
                 resultcount.prepend('<h6 class="text-center">No results found</h6><div class="card-columns">');
@@ -70,48 +70,46 @@ $(document).ready(function() {
         }
         for (var item in result) {
             var ref = result[item].ref;
+            var searchitem;
             if (store[ref].img) {
-                var searchitem =
-                    '<article class="my-2 text-left">' 	+
-                    '<div class="row">'	+
-                    '<div class="col">' +
-                    '<h5 class="chulapa-links-hover-only" itemprop="headline">' +
-                    '<a href="' + store[ref].url + '" rel="permalink">' + store[ref].title + '</a>' +
-                    '</h5>' +
-                    '</div>'		+
-					'<div class="col-4 col-md-3">' +
-					'<a href="' + store[ref].url + '" rel="permalink">' +
-					'<div class="rounded-lg chulapa-overlay-img chulapa-gradient chulapa-min-h-10"'+
-					'style="background-image:' +
-					'url(\'' + store[ref].img + '\')" ></div>' +
-					'</div></a>' +
-                    '</div>' +		
-                    '<div class="row mt-2">' +
-                    '<div class="col">' +
-                    '<p>' + store[ref].excerpt.split(" ").splice(0, 10).join(" ") +
-								'</p>' +
-					'</div>' +
-					'</div>' +
-					'<hr>' +
-					'</article>';
+                searchitem = `
+                    <article class="my-2 text-left">
+                        <div class="row">
+                            <div class="col">
+                                <h5 class="chulapa-links-hover-only" itemprop="headline">
+                                    <a href="${store[ref].url}" rel="permalink">${store[ref].title}</a>
+                                </h5>
+                            </div>
+                            <div class="col-4 col-md-3">
+                                <a href="${store[ref].url}" rel="permalink">
+                                    <div class="rounded-lg chulapa-overlay-img chulapa-gradient chulapa-min-h-10" style="background-image: url('${store[ref].img}')"></div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col">
+                                <p>${store[ref].excerpt}</p>
+                            </div>
+                        </div>
+                        <hr>
+                    </article>`;
             } else {
-                var searchitem =
-                    '<article class="my-2 text-left">' 	+
-                    '<div class="row">'	+
-                    '<div class="col">' +
-                    '<h5 class="chulapa-links-hover-only" itemprop="headline">' +
-                    '<a href="' + store[ref].url + '" rel="permalink">' + store[ref].title + '</a>' +
-                    '</h5>' +
-                    '</div>'		+
-                    '</div>'		+
-                    '<div class="row mt-2">' +
-                    '<div class="col">' +
-                    '<p>' + store[ref].excerpt.split(" ").splice(0, 10).join(" ") +
-								'</p>' +
-					'</div>' +
-					'</div>' +
-					'<hr>' +
-					'</article>';
+                searchitem = `
+                    <article class="my-2 text-left">
+                        <div class="row">
+                            <div class="col">
+                                <h5 class="chulapa-links-hover-only" itemprop="headline">
+                                    <a href="${store[ref].url}" rel="permalink">${store[ref].title}</a>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col">
+                                <p>${store[ref].excerpt.split(" ").splice(0, 10).join(" ")}</p>
+                            </div>
+                        </div>
+                        <hr>
+                    </article>`;
             }
             resultdiv.append(searchitem);
         }
