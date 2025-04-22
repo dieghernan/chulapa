15a6e05b047740f2928cb269b0fd568df3f7cb1e
Diego H. <diego.hernangomezherrero@gmail.com>
2025-04-22 21:30:07 +0200

    Update my themes

--- highlighters.R
+++ highlighters.R
@@ -11,8 +11,8 @@ write_lines(dr, "_sass/highlight/dracula.scss")
 
 sassf <- list.files("_sass/highlight", pattern = ".scss", full.names = TRUE)
 sassf <- sassf[!grepl("template", sassf)]
-i <- 1
 
+i <- 12
 file.path("./docs/assets/css/highlighter") |>
   list.files(full.names = TRUE) |>
   unlink()
@@ -50,8 +50,10 @@ for (i in seq_len(length(sassf))) {
   enc <- guess_encoding(f)$encoding[1]
 
   lns <- read_lines(f, locale = locale(encoding = enc))
-
+  is_my_template <- any(str_detect(lns, "// Entities"))
+  if(!is_my_template){
   lns <- str_replace(lns, fixed(".highlight {"), ".highlight pre {")
+  }
   write_lines(lns, f)
   lns2 <- c("---", "---", "", lns)
   bname <- basename(f)
